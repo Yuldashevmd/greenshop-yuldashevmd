d8e483ec0c096f5e9905f3d45252dae3dba9d591
Muhammad <muxammad_205@mail.ru>
2022-09-08 19:20:53 +0500

    login

--- src/components/shop/cardTotal/index.jsx
+++ src/components/shop/cardTotal/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Container, Link, Total } from "./style";
 import ButtonDiv from "../../genericComp/Button";
 import { ContextWrapper } from "../../context";
--- src/components/shop/checkout/order/index.jsx
+++ src/components/shop/checkout/order/index.jsx
@@ -10,11 +10,7 @@ const Order = ({ info }) => {
   const { countData } = useContext(ContextWrapper);
   const [count] = countData;
   const { Panel } = Collapse;
-  const text = `
-    A dog is a type of domesticated animal.
-    Known for its loyalty and faithfulness,
-    it can be found as a welcome guest in many households across the world.
-  `;
+
   return (
     <Container>
       <h2>Cart Totals</h2>
