c238e2f3fdf3bb1e048773d6942d33730c94cd58
Muhammad <muxammad_205@mail.ru>
2022-09-19 22:52:21 +0500

    plant-care-page

--- src/components/plantCare/index.jsx
+++ src/components/plantCare/index.jsx
@@ -1,10 +1,49 @@
 import React from "react";
-import { Container } from "./style";
+import { Blog, Container } from "./style";
 
 const PlantCare = () => {
   return (
     <Container>
-      <h1>Hello this is PlantCare page</h1>
+      <h1 className="headerText">About Plant Care</h1>
+      <Blog>
+        <h1>House Plants Going Back in Time</h1>
+        <p>
+          The variety of house plants you can grow indoors is amazing. However,
+          this article was written over 50 years ago and shows the true variety
+          of houseplants people use to grow indoors and was available to them.
+          The question is… do we have that same variety available today? You'll
+          notice that these indoor plants.
+        </p>
+        <a href="http://www.plant-care.com/house-plants-going-back-in-time.html">
+          Read full article here...
+        </a>
+      </Blog>
+      <Blog>
+        <h1>Garden Landscaping – A Great Way To Update A Backyard</h1>
+        <p>
+          Garden landscaping is a great way to update a backyard. Giving your
+          backyard a makeover will certainly improve your homes appearance and
+          add value at the same time. Planting new flowers and updating your
+          landscape will add beauty to your home and make a better place for you
+          and your family to spend time in.
+        </p>
+        <a href="http://www.plant-care.com/1526-garden-landscaping.html">
+          Read full article here...
+        </a>
+      </Blog>
+      <Blog>
+        <h1>Climbing Plants – For Privacy and Beauty</h1>
+        <p>
+          Amateur gardeners have neglected some of the best climbers that are
+          grown quickly in almost any climate. One that is seldom seen, in spite
+          of the fact that it was introduced into our country from Japan 100
+          years ago, is the climbing hydrangea. It bears splendid tresses of
+          white flowers in July and August
+        </p>
+        <a href="http://www.plant-care.com/climbing-plants-for-privacy-and-beauty.html">
+          Read full article here...
+        </a>
+      </Blog>
     </Container>
   );
 };
--- src/components/plantCare/style.js
+++ src/components/plantCare/style.js
@@ -2,10 +2,19 @@ import styled from "styled-components";
 
 const Container = styled.div `
   width: 1180px;
-  margin: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  margin: 56px auto 0 auto;
+  .headerText {
+    font-weight: bold;
+    font-size: 28px;
+    text-align: center;
+  }
 `;
-
+export const Blog = styled.div `
+  width: 60%;
+  margin: 50px 0 65px 0;
+  h1 {
+    font-weight: 600;
+    font-size: 20px;
+  }
+`;
 export { Container };
